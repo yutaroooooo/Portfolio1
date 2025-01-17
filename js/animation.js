634ce8b3ffe803895be95ae9929275ce15d6dfc1
Yutaro <yohnuki1@gmail.com>
2025-01-17 21:01:45 +0900

    no message

--- js/animation.js
+++ js/animation.js
@@ -45,27 +45,3 @@ window.addEventListener('load', () => {
     // アニメーション開始
     animate();
 });
-
-// DOMの読み込み後にアニメーション開始
-window.addEventListener('load', () => {
-    const element = document.querySelector('.graphic3');
-    
-    let rotationY = 0; // Y軸回転の初期角度
-
-    function animate() {
-        // Y軸回転のアニメーション
-        rotationY += 1; // 1度ずつ回転
-        if (rotationY >= 360) {
-            rotationY = 0; // 360度を超えたら再び0度に戻る
-        }
-
-        // CSSのtransformプロパティで回転を適用
-        element.style.transform = `rotateY(${rotationY}deg)`;
-
-        // アニメーションを継続
-        requestAnimationFrame(animate);
-    }
-
-    // アニメーション開始
-    animate();
-});
